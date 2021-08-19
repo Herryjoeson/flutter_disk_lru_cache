730026b0658a5408111a6ef23320315cafabff57
jasonlau233 <15622981320@163.com>
2021-08-19 11:32:25 +0800

    alter: update test file

--- test/flutter_disk_lru_cache_test.dart
+++ test/flutter_disk_lru_cache_test.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:crypto/crypto.dart';
 import 'package:flutter_disk_lru_cache/flutter_disk_lru_cache.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,33 +11,25 @@ void main() async {
     () async {
       DiskLruCache diskLruCache = await DiskLruCache.open(Directory("./disk"));
       try {
-        for (int i = 0; i < 20; i++) {
-          final key = md5
-              .convert(
-                utf8.encode(DateTime.now().microsecondsSinceEpoch.toString()),
-              )
-              .toString();
-          Editor? editor = await diskLruCache.edit(key);
-          if (editor == null) {
-            throw Exception("editor为空");
-          }
-          final Uint8List imageBytes = await getBaiduImage();
-          FaultHidingIOSink faultHidingIOSink = editor.newOutputIOSink(0);
-          faultHidingIOSink.write(imageBytes);
-          await faultHidingIOSink.close();
-          await editor.commit(diskLruCache);
+        final key = "1ed614ecb59ddb04e42338ae77f446ef";
+        Editor? editor = await diskLruCache.edit(key);
+        if (editor == null) {
+          throw Exception("editor为空");
         }
+        final Uint8List imageBytes = await getBaiduImage();
+        FaultHidingIOSink faultHidingIOSink = editor.newOutputIOSink(0);
+        await faultHidingIOSink.writeByte(imageBytes);
+        await faultHidingIOSink.flush();
+        await faultHidingIOSink.close();
+        await editor.commit(diskLruCache);
         diskLruCache.flush();
-        Snapshot? snapshot = await diskLruCache.get("25d55ad283aa400af464c76d713c07ad");
+        Snapshot? snapshot = await diskLruCache.get(key);
         if (snapshot == null) {
           print("snapshot对象为空");
           return;
         }
-        RandomAccessFile inV1 = snapshot.getRandomAccessFile(0);
-        Uint8List bytes = inV1.readSync(inV1.lengthSync());
-        diskLruCache.edit(snapshot.key, sequenceNumber: snapshot.sequenceNumber);
-        print(inV1.lengthSync()); // 138656
-        print(bytes);
+        RandomAccessFile randomAccessFile = snapshot.getRandomAccessFile(0);
+        randomAccessFile.readSync(snapshot.getLength(0));
         snapshot.close();
       } catch (e) {
         print(e);
