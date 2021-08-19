c6582b06ded3dfe64eee8b25514e11c121969df7
jasonlau233 <15622981320@163.com>
2021-08-19 11:30:08 +0800

    alter: provider text and bytes to write

--- lib/src/proxy/fault_hiding_io_sink.dart
+++ lib/src/proxy/fault_hiding_io_sink.dart
@@ -4,90 +4,41 @@ part of flutter_disk_lru_cache;
 typedef void ErrorVoidCallback(Object e);
 
 /// 代理类ioSink
-class FaultHidingIOSink implements IOSink {
-  final IOSink sink;
-  final ErrorVoidCallback onError;
+class FaultHidingIOSink {
+  final RandomAccessFile _randomAccessFile;
+  final ErrorVoidCallback _onError;
 
-  FaultHidingIOSink(this.sink, this.onError);
+  FaultHidingIOSink(this._randomAccessFile, this._onError);
 
-  @override
-  void add(List<int> data) {
+  Future<void> write(String text) async {
     try {
-      sink.add(data);
+      await _randomAccessFile.writeString(text);
     } catch (e) {
-      this.onError(e);
+      _onError(e);
     }
   }
 
-  @override
-  void addError(Object error, [StackTrace? stackTrace]) => sink.addError(error, stackTrace);
-
-  @override
-  Future addStream(Stream<List<int>> stream) async {
+  Future<void> writeByte(List<int> buffer, [int start = 0, int? end]) async {
     try {
-      return await sink.addStream(stream);
+      await _randomAccessFile.writeFrom(buffer, start, end);
     } catch (e) {
-      this.onError(e);
+      _onError(e);
     }
   }
 
-  @override
-  Future close() async {
+  Future<void> close() async {
     try {
-      return await sink.close();
+      await _randomAccessFile.close();
     } catch (e) {
-      this.onError(e);
+      _onError(e);
     }
   }
 
-  @override
-  Future get done => sink.done;
-
-  @override
-  Future flush() async {
+  Future<void> flush() async {
     try {
-      return await sink.flush();
+      await _randomAccessFile.flush();
     } catch (e) {
-      this.onError(e);
+      _onError(e);
     }
   }
-
-  @override
-  void write(Object? object) {
-    try {
-      sink.write(object);
-    } catch (e) {
-      this.onError(e);
-    }
-  }
-
-  @override
-  void writeAll(Iterable objects, [String separator = ""]) {
-    try {
-      sink.writeAll(objects, separator);
-    } catch (e) {
-      this.onError(e);
-    }
-  }
-
-  @override
-  void writeCharCode(int charCode) {
-    try {
-      sink.writeCharCode(charCode);
-    } catch (e) {
-      this.onError(e);
-    }
-  }
-
-  @override
-  void writeln([Object? object = ""]) {
-    try {
-      sink.writeln(object);
-    } catch (e) {
-      this.onError(e);
-    }
-  }
-
-  @override
-  late Encoding encoding = utf8;
 }
